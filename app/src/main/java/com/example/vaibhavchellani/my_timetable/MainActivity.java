96d45a62465987477d54f8cf7b4e8064b87c8ee8
vaibhavchellani <vaibhavchellani223@gmail.com>
2017-02-18 22:54:13 +0530

    side menu added

--- app/src/main/java/com/example/vaibhavchellani/my_timetable/MainActivity.java
+++ app/src/main/java/com/example/vaibhavchellani/my_timetable/MainActivity.java
@@ -9,6 +9,9 @@ import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,25 +25,16 @@ import com.example.vaibhavchellani.my_timetable.database.AttedanceDB;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-import static com.example.vaibhavchellani.my_timetable.R.id.button_contact;
+
 
 public class MainActivity extends AppCompatActivity {
     private Spinner spinner_year,spinner_branch,spinner_shift;
 
     private TextView textview;
 
-    private static final int uniqueID = 45612;
-
-    private TextView textView_year,textView_branch,textView_shift;
     AttedanceDB db;
 
     private Button button_contact,start_notif_button;
-
-
-    private static final String[] year = {"None Selected", "I Year", "II Year", "III Year ", "IV year", "Extra year ;)"};
-    private static final String[] branch = {"None Selected", "IT", "CSE", "ECE", "ICE"};
-    private static final String[] shift = {"None Selected", "Evening Shift","Morning Shift"};
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,19 +44,12 @@ public class MainActivity extends AppCompatActivity {
         db.addrow_check_class();
         db.make_attendance_table();
 
-        button_contact=(Button)findViewById(R.id.button_contact);
-        button_contact.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //todo to put email option
-                composeEmail("TIMETABLE APP FEEDBACK");
-            }
-        });
 
         findViewById(R.id.startnotifs_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 createAlarms();
+                Toast.makeText(MainActivity.this, " Notification Service Activated For Today", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -85,95 +72,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.delete_databse_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                db.deleteTable(db.TABLE_ATTENDANCE);
-
-            }
-        });
-
-
-
-        /*textView_year=(TextView)findViewById(R.id.select_year_textview);
-        textView_branch=(TextView)findViewById(R.id.select_branch_textview);
-        textView_shift=(TextView)findViewById(R.id.select_shift_textview);
-
-        spinner_year=(Spinner)findViewById(R.id.spinner_year);
-        spinner_branch=(Spinner)findViewById(R.id.spinner_branch);
-        spinner_shift=(Spinner)findViewById(R.id.spinner_shift);
-
-
-        ArrayAdapter<String> adapter_year=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,year);
-        ArrayAdapter<String> adapter_branch=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,branch);
-        ArrayAdapter<String> adapter_shift=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,shift);
-
-
-        adapter_year.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_year.setAdapter(adapter_year);
-        spinner_year.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if (i == 0) {
-                    //do nothing
-                }
-                else {
-                    String item_year = adapterView.getItemAtPosition(i).toString();
-                    Toast.makeText(MainActivity.this, "hi the item is " + item_year, Toast.LENGTH_SHORT).show();
-                    //load up timetable for respective year
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-                //do nothing
-            }
-        });
-
-        adapter_branch.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_branch.setAdapter(adapter_branch);
-        spinner_branch.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if (i == 0) {
-                    //do nothing
-                }
-                else {
-                    String item_branch = adapterView.getItemAtPosition(i).toString();
-                    Toast.makeText(MainActivity.this, "hi the item is " + item_branch, Toast.LENGTH_SHORT).show();
-                    //load up timetable for respective year
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-                //do nothing
-            }
-        });
-
-
-        adapter_shift.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_shift.setAdapter(adapter_shift);
-        spinner_shift.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if (i == 0) {
-                    //do nothing
-                }
-                else {
-                    String item_shift = adapterView.getItemAtPosition(i).toString();
-                    Toast.makeText(MainActivity.this, "hi the item is " + item_shift, Toast.LENGTH_SHORT).show();
-                    //load up timetable for respective year
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-                //do nothing
-            }
-        });*/
     }
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.landingpge_menu, menu);
+        return true;
+    }
+    
     public void deleteDatabase(View view) {
         Toast.makeText(this, "we are inside delete", Toast.LENGTH_SHORT).show();
 
@@ -203,6 +109,21 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.contactus:
+                composeEmail("Timetable app feedback");
+                return true;
+            case R.id.resetDatabse:
+                db.deleteTable(db.TABLE_ATTENDANCE);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     public void createAlarms(){
 
         Calendar calendar=Calendar.getInstance();
